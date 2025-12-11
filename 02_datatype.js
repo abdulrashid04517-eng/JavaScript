f4209f69b7ab965db3d842bd0cd818ed6c999208
cogniv <abdulrashid04517@gmail.com>
2025-12-11 16:12:55 +0530

    Initial

---
+++ 02_datatype.js
@@ -0,0 +1,53 @@
+//treat all JS code as newer version //
+
+// alert(3+3)  // We are using node js , not browser
+
+ console.log(3 
+ 	+ 3)   //code readability should be high
+
+ console.log("Rashid")
+
+
+ let name ="Rashid"     // string datatype
+ let age = 18           // number datatype
+ let isLoggedIn =false  // boolean datatype
+
+
+//num<=2 to power 53-1
+//bigint for big numbers
+//string ="" (in string we use double code even if we wrote a0 no. in double code then the type of that is a string not no. because of double code)
+//boolean = true/false
+//null = standalone value 
+//undefiened = value not defined
+//symbol = unique
+//object = most famous datatype
+//typeof = for knowing the type of datatype(for writing the typeof there are two methods 1.typeof 2.(typeof))
+//type of null = object (known as the error most famous error of this language)
+
+//console.log(typeof null);
+//NaN (for that condition if we give the wrong input in datatype(except:boolean/null))
+//"33" = 33
+//"33abc" = NaN
+
+
+// let score = "Rashid"
+
+// console.log(typeof score);
+// console.log((typeof score));
+
+
+// let ValueInNumber = Number(score)
+// console.log(typeof  ValueInNumber);
+// console.log(ValueInNumber);
+//true = 1 or any value ("Rashid")
+//false = 0 or empty ("")
+
+// let isLoggedIn = "Rashid"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+

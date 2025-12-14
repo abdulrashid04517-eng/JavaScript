bcb5664766524ae48561936a5e9748ae83266dd3
cogniv <abdulrashid04517@gmail.com>
2025-12-14 15:12:39 +0530

    initial4

---
+++ 07_num_and_math.js
@@ -0,0 +1,52 @@
+
+const score = 400
+console.log(score);
+
+const balance = new Number(1000)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(4));
+
+
+const otherNumber = 1981.9875
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// max value = 1.7976931348623157e+308
+// min value = 5e-324
+// max safe integer = 9007199254740991
+
+
+// +++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(8.9));
+console.log(Math.ceil(8.1));
+console.log(Math.floor(8.8));
+console.log(Math.min(8,9,7,6,2));
+console.log(Math.max(2,3,4,8,));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random()*(max - min +1)) + min);
+
+
+
+
+
+
+
+
+
+
+
